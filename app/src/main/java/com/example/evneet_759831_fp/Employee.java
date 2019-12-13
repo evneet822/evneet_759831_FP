9d70f852e8eb4e1f30f7fc69cb2ebabfdfc0481f
evneet <nancymaan4@gmail.com>
2019-12-12 20:33:16 -0500

    added methods in employee class

--- app/src/main/java/com/example/evneet_759831_fp/Employee.java
+++ app/src/main/java/com/example/evneet_759831_fp/Employee.java
@@ -1,5 +1,7 @@
 package com.example.evneet_759831_fp;
 
+import java.util.ArrayList;
+
 public class Employee {
 
     private String firstName,lastName;
@@ -8,14 +10,64 @@ public class Employee {
     private int empID;
     private Vehicle vehicle;
 
+    public static ArrayList<Employee> employeeDetails = new ArrayList<>();
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public double getOccRate() {
+        return occRate;
+    }
+
+    public int getEmpID() {
+        return empID;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
     public Employee(String firstName, String lastName, int birthYear, double salary, double occRate, int empID, Vehicle vehicle) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthYear = birthYear;
         this.salary = salary;
-        this.occRate = occRate;
+        this.occRate = occRate <= 10.0 ? 10.0 : (occRate >= 100.0 ? 100.0 :occRate );
         this.empID = empID;
         this.vehicle = vehicle;
         this.age = 2019 - birthYear;
     }
+    public Employee(String firstName, String lastName, int birthYear, double salary, int empID, Vehicle vehicle) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.salary = salary;
+        this.occRate = 100.0;
+        this.empID = empID;
+        this.vehicle = vehicle;
+        this.age = 2019 - birthYear;
+    }
+
+    public double AnnualIncome(){
+
+        return salary * 12;
+    }
+
 }

6cbaef5961c207fd30d252c76b8f2ac0f75d83c6
evneet <nancymaan4@gmail.com>
2019-12-12 20:03:47 -0500

    vehicle class

---
+++ app/src/main/java/com/example/evneet_759831_fp/Vehicle.java
@@ -0,0 +1,18 @@
+package com.example.evneet_759831_fp;
+
+public class Vehicle {
+
+    private String make;
+    private String plateNumb;
+    private String color;
+
+
+    public Vehicle(String make, String plateNumb, String color) {
+        this.make = make;
+        this.plateNumb = plateNumb;
+        this.color = color;
+
+    }
+
+
+}

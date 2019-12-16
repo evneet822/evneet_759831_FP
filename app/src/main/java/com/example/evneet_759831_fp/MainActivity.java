572c3f78a8e9b7bdae752fe97ace85a1441e3ba9
evneet <nancymaan4@gmail.com>
2019-12-15 21:29:48 -0500

    listner for listview

--- app/src/main/java/com/example/evneet_759831_fp/MainActivity.java
+++ app/src/main/java/com/example/evneet_759831_fp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -33,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this,DescriptionActivity.class);
                 startActivity(intent);
             }
         });
